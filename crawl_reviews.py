a53d60c446c3ac0f594c7d5e7a21e85f76abd53f
HongyeJin <jhy0410@tamu.edu>
2022-11-09 17:45:42 -0800

    change api and id

--- crawl_reviews.py
+++ crawl_reviews.py
@@ -1,27 +1,31 @@
+import time
 import pandas as pd
 from tqdm import tqdm
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
-# change driver path according to your configuration
-path = "/snap/bin/chromium.chromedriver"
-driver = webdriver.Chrome(path)
-driver.get('https://openreview.net/group?id=ICLR.cc/2022/Conference')
+
+
 
 df = pd.read_csv('paperlist.tsv', sep='\t', index_col=0)
 
+driver.get('https://openreview.net/group?id=NeurIPS.cc/2022/Conference')
+#driver.get('https://openreview.net/group?id=ICLR.cc/2022/Conference')
+#driver = webdriver.Chrome()
+
 ratings = dict()
 decisions = dict()
-for paper_id, link in tqdm(list(df.link.items())):
+for paper_id, link in tqdm(list(df.link.items())):s
     try:
         driver.get(link)
         xpath = '//div[@id="note_children"]//span[@class="note_content_value"]/..'
         cond = EC.presence_of_element_located((By.XPATH, xpath))
         WebDriverWait(driver, 60).until(cond)
 
-        elems = driver.find_elements_by_xpath(xpath)
+        elems = driver.find_elements("xpath", xpath)
         assert len(elems), 'empty ratings'
         ratings[paper_id] = pd.Series([
             int(x.text.split(': ')[1]) for x in elems if x.text.startswith('Recommendation:')
